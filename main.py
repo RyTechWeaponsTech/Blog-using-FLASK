5c22517de6b50cbb1c8bd614d800c9f683b6a074
archivedghost001 <aryamuhammadkamil@gmail.com>
2024-08-27 20:32:53 +0700

    Second commit

--- main.py
+++ main.py
@@ -48,3 +48,112 @@ def index():
         print("Success!!!")
 
     return render_template("index.html", article=article, name=name)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/addpost", methods=["POST", "GET"])
+def addpost():
+    if request.method == "POST":
+        title = request.form["title"]
+        author = request.form["author"]
+        content = request.form["content"]
+
+        post = Riley_blog(
+            title=title, author=author, content=content, post_date=datetime.now()
+        )
+
+        db.session.add(post)
+        db.session.commit()
+        print("Done")
+
+        return redirect(url_for("index"))
+    return render_template("add.html")
+
+
+@app.route("/update/<int:id", methods=["POST", "GET"])
+@login_required
+def update(id):
+    if request.method == "POST":
+        title = request.form["title"]
+        author = request.form["author"]
+        content = request.form["content"]
+        print(content)
+
+        post = Riley_blog.query.filter_by(id=id).first()
+
+        post.title = title
+        post.author = author
+        post.content = content
+
+        db.session.add(post)
+        db.session.commit()
+        return redirect("/")
+
+    edit = Riley_blog.query.filter_by(id=id).first()
+
+    return render_template("update.html", edit=edit)
+
+
+@app.route("/delete/<int: id>")
+@login_required
+def delete(id):
+    data = Riley_blog.query.filter_by(id=id).first()
+
+    db.session.delete(data)
+    db.session.commit()
+
+    return redirect(url_for("index"))
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        print("login")
+        username = request.form("username")
+        password = request.form("password")
+        user = User.query.filter_by(username=username).first()
+
+        if not user and not check_password_hash(user.password, password):
+            flash("Wrong username/password :skull:")
+
+            return render_template("not.html")
+    else:
+        login_user(user)
+
+        print("Successfully logged in")
+        return redirect(url_for("index.html"))
+
+    return render_template("login.html")
+
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    if request.method == "POST":
+        print("Attempt to sign up!")
+        username = request.form("username")
+        password = request.form("password")
+
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for("index"))
+
+    return render_template("signup.html")
+
+
+@app.route("/logout", methods=["POST", "GET"])
+@login_required
+def logout():
+    login_user()
+
+    return redirect(url_for("index"))
+
+
+# main driver function
+if __name__ == "__main__":
+    app.run(debug=True)
